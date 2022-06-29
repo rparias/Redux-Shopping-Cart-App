d15eb112db8c92e1f4460fa0e8324d12baf2da73
Ronald Arias <ariasron@hotmail.com>
2022-06-29 16:03:53 -0500

    writes auth slice reducer to handle authentication

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {isLoggedIn ? <Layout/> : <Auth />}
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,20 @@
-import React from "react";
+import { useDispatch } from 'react-redux'
+import { login } from '../store/authSlice'
 
 import "./Auth.css";
 
 const Auth = () => {
+  const dispatch = useDispatch()
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    dispatch(login())
+  }
+
   return (
     <div className="container">
       <h1>Login</h1>{" "}
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <label htmlFor="id">Id</label>
         <input type="text" name="id" id="id" />
         <label htmlFor="password">Password</label>
